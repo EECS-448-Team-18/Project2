8143c2279ca373af9ce55ec0cb8c7979d6c5bf62
gholmes829 <g.holmes429@gmail.com>
2020-09-03 18:55:24 -0500

    adding to structure

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import sys
+from game import Game
+
+def __main__():
+	game = Game()
+	game.run()
+
+if __name__ == "__main_":
+	main()
+	sys.exit()
---
+++ data/settings.py
@@ -0,0 +1,3 @@
+game_name = "Battleship"
+screen_size = (600, 600)
+target_frame_rate = 60
