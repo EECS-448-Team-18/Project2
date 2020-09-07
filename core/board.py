7eb5dceee46d7ddce8f4f1a841721247a4eeeead
Peyton Doherty <p983d082@ku.edu>
2020-09-07 16:29:42 -0500

    Tried to turn previous code into class

--- core/board.py
+++ core/board.py
@@ -1,84 +1,60 @@
-#"""
-#Game board stuff
-#"""
-
-#class Board:
-
-	#def __init__(self):
-		#pass
-
+"""
+Game board stuff
+"""
 import pygame
- 
-#Colors
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
- 
-#Height and Width of each square
-width = 50
-height = 50
- 
-#Spacing between each square
-spacing = 5
+import engine
 
-# Creates a 2 dimensial array
-grid = []
-for row in range(9):
-    grid.append([])
-    for column in range(9):
-        grid[row].append(0)  
+class Board:
 
-grid2 = []
-for row in range(9):
-    grid2.append([])
-    for column in range(9):
-        grid2[row].append(0)
+	def __init__(self):
+        self.grid = []
+        self.grid2 = []
+        self.width = 50
+	    self.height = 50
+        self.spacing = 5
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.green = (0, 255, 0)
+		self.create_grid()
+        self.draw_grid()
+        self.mouseclick()
+    
+    #This is building the 2-D array
+    def create_grid(self):
+        for row in range(9):
+            self.grid.append([])
+            for column in range(9):
+                self.grid[row].append(0)  
+
+        for row in range(9):
+            self.grid2.append([])
+            for column in range(9):
+                self.grid2[row].append(0) 
+    
+    #This is drawing the grid
+    def draw_grid(self):
+        for row in range(9):
+        for column in range(9):
+            color = white
+            if grid[row][column] == 1:
+                color = green
+            pygame.draw.rect(self.engine.screen, color, [ (self.spacing + self.width) * column + self.spacing,
+                            (self.spacing + self.height) * row + self.spacing, self.width, self.height])
+
+    def mouseclick(self):
+        clock = pygame.time.Clock()
  
-pygame.init()
- 
-# Setting the size of the screen
-display = [1200, 600]
-screen = pygame.display.set_mode(display)
- 
-# Setting the title
-pygame.display.set_caption("Battleship Grid")
- 
-done = False
- 
-# Manages how fast the screen updates
-clock = pygame.time.Clock()
- 
-while not done:
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
-            done = True  # This will allow the user to exit
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        #if the user doesnt quit then the while loop continues
+        while self.engine.handle_events == false:
+            #handle events function in engine.py
+            event.type == pygame.MOUSEBUTTONDOWN:
             # User clicks the mouse. Get the position
             pos = pygame.mouse.get_pos()
-            # Change the x/y screen coordinates to grid coordinates
+            # Change the x and y coordinates screen coordinates to grid coordinates
             column = pos[0] // (width + spacing)
             row = pos[1] // (height + spacing)
             # Set that location to one
             grid[row][column] = 1
             print("Click ", pos, "Grid coordinates: ", row, column)
- 
-    # Makes the background black
-    screen.fill(black)
- 
-    # Draws both grids
-    for row in range(9):
-        for column in range(9):
-            color = white
-            if grid[row][column] == 1:
-                color = green
-            pygame.draw.rect(screen,color,[ (spacing + width) * column + spacing,
-                            (spacing + height) * row + spacing, width, height])
- 
-    # Limits to 60 FPS
-    clock.tick(60)
- 
-    # Updates the screen
-    pygame.display.flip()
- 
-pygame.quit()
+
+
