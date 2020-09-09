00ab9310c67951386090303cc7f77d334aa683aa
Peyton Doherty <p983d082@ku.edu>
2020-09-09 17:21:27 -0500

    Finished place and set function and get coordinates function

--- core/board.py
+++ core/board.py
@@ -7,20 +7,39 @@ class Board:
 
 	def __init__(self):
         self.grid = []
-        self.grid2 = []
-		self.create_grid()
+        self.get_coordinates()
+        self.place_and_set()
     
-    #This is building the 2-D array
-    def create_grid(self):
-        for row in range(9):
+        #Creates a 2D array with 0s stored in each index
+        for row in range(10):
             self.grid.append([])
-            for column in range(9):
+            for column in range(10):
                 self.grid[row].append(0)  
 
-        for row in range(9):
-            self.grid2.append([])
-            for column in range(9):
-                self.grid2[row].append(0) 
+    #Wants to return if there is a ship in that position
+    def get_coordinates(self, x, y)->grid position:
+        #checks to see if the coordinates are valid
+        if((x > 9 or x < 0) or (y > 9 or y < 0)):
+            return (False)
+        else:
+            #Will return the value at that given coordinate
+            for i in range(10):
+                for j in range(10):
+                    if(i == x and j == y):
+                        return self.grid[i][j]
+    
+    #Changes the number in the grid to 1 in order represent that there is a ship on that positon of the board
+    def place_and_set(self, x, y):
+        if((x > 9 or x < 0) or (y > 9 or y < 0)):
+            #checks to see if the coordinates are valid
+            return(False)
+        else:
+            #Changes the value of the coordinates given
+            for i in range(10):
+                for j in range(10):
+                    if(i == x and j == y):
+                        self.grid[i][j] = 1
+        
     
     
 
